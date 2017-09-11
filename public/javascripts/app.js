690344994f256f44fb1b3577d77cb8d2867d60d3
Lana Reeve <lreeve@prevalent.net>
2017-09-11 15:37:19 -0400

    initial junk

---
+++ public/javascripts/app.js
@@ -0,0 +1,86 @@
+const mutators = {
+    capitalize: str => str.charAt(0).toUpperCase() + str.slice(1),
+    humanize: (prop, value) => {
+        if (prop === 'bulk' && value === 0) return 'L';
+        else if (value === null) return '-';
+        return value && mutators.capitalize( value.toString() );
+    }
+}
+
+class ArmoryService {
+    constructor (data) {
+        this.data = data;
+        this.defaultSort = {type: 1, level: 1, price: 1};
+    }
+
+    getCategories () {
+        if (this.data && this.data.categories) return Object.keys(this.data.categories);
+        return [];
+    }
+
+    getSubcategories ( filter ) {
+        const category = this.data && this.data.categories && this.data.categories[ filter.category ];
+        if (category) return Object.keys(category);
+        return [];
+    }
+
+    getProperties ( filter ) {
+        const category = this.data && this.data.categories && this.data.categories[ filter.category ];
+        const subcategory = category && category[ filter.subcategory ];
+        if (subcategory) return Object.keys( subcategory[ Object.keys( subcategory )[0] ] );
+        return [];
+    }
+
+    getItems ( filter ) {
+        const category = this.data && this.data.categories && this.data.categories[ filter.category ];
+        const subcategory = category && category[ filter.subcategory ];
+        return subcategory || [];
+    }
+}
+
+const App = new Vue({
+    el: "#app-body",
+    data: {
+        armory: new ArmoryService(),
+        loading: false,
+        showHeader: true,
+        filter: {},
+        mutate: mutators
+    },
+
+    methods: {
+        updateFilter: function( newFilter ) {
+            for (const key in newFilter) {
+                this.filter[key] = newFilter[key];
+            }
+
+            this.$forceUpdate();
+        }
+    }
+});
+
+const ArmoryLoaderService = new Vue({
+    el: "#loader",
+    data: {
+        loading: false
+    },
+
+    methods: {
+        startLoad: function() {
+            ArmoryLoaderService.loading = true;
+
+            $.getJSON( "data/armory.json", function ( data ) {
+                App.armory = new ArmoryService(data);
+                App.loading = false;
+                ArmoryLoaderService.loading = false;
+            });
+        }
+    },
+
+    mounted: function() {
+        this.$nextTick(function() {
+            this.startLoad();
+        });
+    }
+});
+
